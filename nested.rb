54da890a00b0e11671fb51489fe8a5589c75fdd5
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-15 01:43:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,8 @@ def changing_alan
      
 alans_new_info = "GUI"
 programmer_hash[:alan_key][:known_for] = alans_new_info
-programmer_hash[:alan_key]
+
+programmer_hash
 end
 
 def adding_to_dennis
@@ -123,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
+programmer_hash
 end
