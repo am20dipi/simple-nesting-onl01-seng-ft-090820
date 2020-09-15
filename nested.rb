44b4d0a80299a5ec079da6a24f7e1aa09a43df39
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-15 01:40:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-programmer_hash[:alan_key][:known_for] = "GUI"
+alan_new_lang = "GUI"
+programmer_hash[:alan_key][:known_for] = alan_new_lang
 programmer_hash
 end
 
